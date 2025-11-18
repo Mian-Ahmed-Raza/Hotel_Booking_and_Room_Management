6bf9b757cd455677e33baf2ca6bdaecd6567586d
hadiikhan9222@gmail.com <hadiikhan9222@gmail.com>
2025-11-18 01:30:43 -0800

    added code

---
+++ app/services/db.py
@@ -0,0 +1,24 @@
+# app/services/db.py
+import mysql.connector
+from mysql.connector import Error
+from contextlib import contextmanager
+import os
+
+
+DB_CONFIG = {
+'host': os.getenv('DB_HOST', 'localhost'),
+'user': os.getenv('DB_USER', 'root'),
+'password': os.getenv('DB_PASS', ''),
+'database': os.getenv('DB_NAME', 'hotel_db'),
+}
+
+
+@contextmanager
+def get_connection():
+conn = None
+try:
+conn = mysql.connector.connect(**DB_CONFIG)
+yield conn
+n finally:
+if conn and conn.is_connected():
+conn.close()
